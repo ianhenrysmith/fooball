d0d0a0fb90b8843bf4a8a9e5820fd83c1cdd8ace
Ian Smith <ianhenrysmith@gmail.com>
2014-08-26 19:37:39 -0600

    ok going to cloud storage

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,24 @@
 
 CarrierWave.configure do |config|
-  config.storage = :file
-  config.root = Rails.root.join('tmp')
-  config.cache_dir = "uploads"
+
+  if Rails.env.development?
+    config.storage = :file
+    config.root = Rails.root.join('tmp')
+    config.cache_dir = "uploads"
+
+  else
+    config.storage = :fog
+
+    config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region:                'us-east-1'
+    }
+    config.asset_host     = "http://#{ENV['AWS_BUCKET']}.s3.amazonaws.com"
+    config.fog_directory  = ENV['AWS_BUCKET']
+    config.fog_public     = true
+    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
+    
+  end
 end
